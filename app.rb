96fd2142a2c38f8dcfb5f112b6e57501884c3802
joshweinstock <jyweinstock@uchicago.edu>
2025-01-29 20:24:36 +0000

    dynamic_dice2

--- app.rb
+++ app.rb
@@ -5,62 +5,18 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
-  @rolls = []
 
-  2.times do
-    die = rand(1..6)
 
-    @rolls.push(die)
-  end
+get("/dice/:zebra/:sides") do
+  @num_dice=params.fetch("zebra").to_i
+  @num_sides=params.fetch("sides").to_i
+@rolls= []
 
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
-end
-
-get("/dynamic/50/6") do
-@rolls=[]
-
-50.times do
-  die= rand(1..6)
+@num_dice.times do
+  die = rand(1..@num_sides)
   @rolls.push(die)
 end
 
-"#{@rolls}"
+erb(:flexible)
 
 end
